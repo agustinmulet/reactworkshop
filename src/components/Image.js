34f0da825a584bdb950a32f49b0b8342503ce3e9
Agustin Mulet <agustin.mulet@gmail.com>
2021-12-30 20:16:32 -0300

    Fix para imagenes en gh pages

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,38 +1,41 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
-import { GatsbyImage } from "gatsby-plugin-image";
+import { StaticImage } from "gatsby-plugin-image"
 
-/*
- * This component is built using `gatsby-plugin-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `StaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-plugin-image`: https://www.gatsbyjs.com/docs/how-to/images-and-media/using-gatsby-plugin-image/
- * - `StaticQuery`: https://www.gatsbyjs.com/docs/how-to/querying-data/static-query/
- */
+const Diagrama = () => {
+  return <StaticImage
+            src="../images/diagrama.png"
+            placeholder="blurred"
+            alt="Ciclos de vida de un método"
+        />
+}
 
-const Image = ({ imgName, alt }) => (
-  <StaticQuery
-    query={graphql`
-      query {
-        allImageSharp {
-          nodes {
-            gatsbyImageData
-          }
-        }
-      }
-    `}
-    render={(data) => {
-      const image = data.allImageSharp.nodes.find(
-        (node) => node.gatsbyImageData.images.fallback.src.includes(imgName),
-      )
-      if (!image) {
-        return null
-      }
-      return <GatsbyImage image={image.gatsbyImageData} alt={alt} placeholder="blurred" />;
-    }}
-  />
-)
-export default Image
+const Props = () => {
+  return <StaticImage
+            src="../images/props.jpg"
+            placeholder="blurred"
+            alt="Explicación de Props"
+        />
+}
+
+const State = () => {
+  return <StaticImage
+            src="../images/state.jpg"
+            placeholder="blurred"
+            alt="Explicación de State"
+        />
+}
+
+const Top = () => {
+  return <StaticImage
+            src="../images/top.png"
+            placeholder="blurred"
+            alt="Parte de arriba terminada"
+        />
+}
+
+export {
+  Diagrama,
+  Props,
+  State,
+  Top
+}
